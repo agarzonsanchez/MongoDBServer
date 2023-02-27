25135cccdd13eaae89001c9a108143d9c1938a35
Andres Garzon <agarzonsanchez90@gmail.com>
2023-02-27 15:08:01 -0500

    chatbot first commit

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import ChatBot from "react-simple-chatbot";
+import { ThemeProvider } from "styled-components";
+// import { Segment } from "semantic-ui-react";
 
 const HomePage = () => {
-  return <h1>Holi</h1>;
+  const theme = {
+    background: "#f5f8fb",
+    fontFamily: "Helvetica Neue",
+    headerBgColor: "#EF6C00",
+    headerFontColor: "#fff",
+    headerFontSize: "15px",
+    botBubbleColor: "#EF6C00",
+    botFontColor: "#fff",
+    userBubbleColor: "#fff",
+    userFontColor: "#4a4a4a",
+  };
+  const steps = [
+    {
+      id: "Greet",
+      message: "Hello, Welcome to our website",
+      trigger: "Ask Name",
+    },
+    {
+      id: "Ask Name",
+      message: "Please enter your name",
+      trigger: "waiting",
+    },
+    {
+      id: "waiting",
+      user: true,
+      trigger: "Name",
+    },
+    {
+      id: "Name",
+      message: `Hi {previousValue}, Please select your issue`,
+      trigger: "issues",
+    },
+    {
+      id: "issues",
+      options: [
+        { value: "React", label: "React", trigger: "React" },
+        { value: "Vue", label: "Vue", trigger: "Vue" },
+      ],
+    },
+    {
+      id: "React",
+      message: "Thanks for telling your react issue",
+      end: true,
+    },
+    {
+      id: "Vue",
+      message: "Thanks for telling your vue issue",
+      end: true,
+    },
+  ];
+  return (
+    <>
+      <h1>Holi</h1>
+
+      <div className="d-flex justify-content-end align-items-end">
+        <ThemeProvider theme={theme}>
+          <ChatBot steps={steps} floating="true" />
+        </ThemeProvider>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
